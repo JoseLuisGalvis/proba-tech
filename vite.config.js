d0250804d61e6658d1060833bff77b34d85a71d8
proyectos-jogal <jogal1965jjgh@gmail.com>
2025-10-24 17:44:41 -0300

    Ajustes

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/proba-tech-software/",
+  base: "/proba-tech/",
   plugins: [react()],
 });
